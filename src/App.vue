5d90e439e228e5b50b4b7390ce08b2d791a54943
nadia_lovely_29rpl <nadia_lovely_29rpl@student.smktelkom-mlg.sch.id>
2023-04-09 12:38:22 +0700

    card simple animation

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,59 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <p className="text-3xl text-center font-bold py-4">
+    Belajar enimesyen anjaayyyy
+  </p>
+
+  <p className="text-center my-4">ini contoh yang salah kidss</p>
+  <div
+    className="w-40 h-40 bg-slate-700 mx-auto rounded-lg shadow-md hover:scale-50 hover:rotate-180 origin-top-left hover:bg-slate-400 transition-all"
+  ></div>
+
+  <p className="text-center my-4">ini contoh yang btul rill no fek</p>
+  <div className="w-40 h-40 bg-blue-300 group mx-auto">
+    <div
+      className="w-40 h-40 bg-slate-700 mx-auto rounded-lg shadow-md hover:scale-50 group-hover:rotate-180 origin-top-left hover:bg-slate-400 transition-all"
+    ></div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+
+  <p className="text-center my-8">ini yang kepikiran aj ccc hyunk</p>
+  <div className="relative w-40 h-40 mx-auto overflow-hidden border-violet-600">
+    <div className="w-40 h-40 bg-blue-300 group mx-auto rounded-lg">
+      <div className="w-40 h-40">
+        <div
+          className="z-1000 w-40 h-40 bg-slate-700 mx-auto rounded-lg shadow-md group-hover:translate-x-full transition-all"
+        >
+          <p className="text-white text-center mt-16 absolute left-0 right-0">
+            Coba hover
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div className="w-40 h-40 bg-blue-300 group mx-auto rounded-lg my-10">
+    <div
+      className="w-40 h-40 bg-slate-700 mx-auto rounded-lg shadow-md group-hover:translate-x-full transition-all"
+    ></div>
+  </div>
+
+  <div className="group w-40 h-40 mx-auto mb-10">
+    <div className="w-40 h-40 bg-blue-300 mx-auto rounded-lg">
+      <p className="text-center text-xl">Baaaa!!</p>
+    </div>
+    <div className="relative -top-40 w-40 h-40 bg-slate-700 mx-auto rounded-lg group-hover:translate-x-full transition-all">
+      <p className="text-center text-xl text-white">Ciluk</p>
+    </div>
+  </div>
+
+  <div className="overflow-hidden group w-40 h-40 mx-auto mb-10">
+    <div className="w-40 h-40 bg-blue-300 mx-auto rounded-lg">
+      <p className="text-center text-xl">Baaaa!!</p>
+    </div>
+    <div className="relative -top-40 w-40 h-40 bg-slate-700 mx-auto rounded-lg group-hover:translate-x-full transition-all">
+      <p className="text-center text-xl text-white">Ciluk</p>
+    </div>
+  </div>
+
 </template>
 
 <style scoped>
